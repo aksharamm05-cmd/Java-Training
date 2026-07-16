034704f525ef3f93927704a901f7b800edee5f22
aksharamm05-cmd <aksharamm05@gmail.com>
2026-07-16 20:35:21 +0530

    latest

---
+++ Day14/MultipleCatchDemo.java
@@ -0,0 +1,12 @@
+public class MultipleCatchDemo {
+    public static void main(String[] args) {
+        try {
+            int[] arr = {1,2};
+            System.out.println(arr[5]);
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Error");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Error");
+        }
+    }
+}
---
+++ Day14/NullDemo.java
@@ -0,0 +1,10 @@
+public class NullDemo {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("Null Object");
+        }
+    }
+}
